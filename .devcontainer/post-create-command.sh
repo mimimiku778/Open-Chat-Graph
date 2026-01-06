9b3dad953c8aac69d6f4eee375fa5a4c42ddbfb3
mimimiku778 <0203.sub@gmail.com>
2026-01-07 00:43:35 +0900

    sudoコマンドを追加し、vscodeユーザーにNOPASSWD権限を付与しました。

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -1,5 +1,5 @@
-sed -i 's/host\.docker\.internal/172.17.0.1/g' /usr/local/etc/php/php.ini
-service apache2 reload
+sudo sed -i 's/host\.docker\.internal/172.17.0.1/g' /usr/local/etc/php/php.ini
+sudo service apache2 reload
 
 cd /var/www/html
 composer install
